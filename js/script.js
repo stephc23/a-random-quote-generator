30d5b95d05cbce1b60a3a7b920e8a851ddbbdb57
stephc23 <88057766+stephc23@users.noreply.github.com>
2021-07-28 18:21:36 -0400

    Added quotes array

--- js/script.js
+++ js/script.js
@@ -10,8 +10,35 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
-
+const quotes = [
+{
+  quote: 'Yet what is any ocean but a multitude of drops?',
+  source: 'David Mitchell',
+  citation: 'Cloud Atlas',
+  year: 2004
+},
+{
+  quote: 'In the middle of winter I at last discovered that there was in me an invincible summer.',
+  source: 'Albert Camus',
+  citation: 'Return to Tipasa',
+  year: 1952
+},
+{
+  quote: "I would follow this road wherever it led me. I’d ignore all the others that crossed its path, no matter how intriguing or promising they looked. I’d finally become convinced that if I didn’t, I’d only walk an endless maze.",
+  source: 'Cheryl Strayed',
+  citation: 'Wild',
+  year: 2012
+},
+{
+  quote: "An original idea. That can't be too hard. The library must be full of them.",
+  source: 'Stephen Fry'
+},
+{
+  quote: 'If you type Google into Google, you can break the internet.',
+  source: 'Jen Barber',
+  citation: 'The I.T. Crowd'
+}
+];
 
 /***
  * `getRandomQuote` function
